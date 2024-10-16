67911d340417d5fb719339054bbf3e07a2c4d2aa
Anton <golvcenkoanton@gmail.com>
2024-10-16 21:39:44 +0200

    Homework5

---
+++ Homework5/YulTask3.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract SubOverflow {
+    // Modify this function so that on overflow it returns the value 0
+    // otherwise it should return x - y
+    function subtract(uint256 x, uint256 y) public pure returns (uint256 result) {
+        // Write assembly code that handles overflows
+        assembly {
+            if lt(x, y) {
+                result := 0
+            }
+            result := sub(x, y)
+        }
+    }
+}
